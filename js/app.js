4c24c914a79bda92d4be5f0cc07f08839e4ea3c5
rubenaguilardev <ragui56@wgu.edu>
2025-03-04 15:36:30 -0800

    add user interaction

--- js/app.js
+++ js/app.js
@@ -1,16 +1,50 @@
-let container = document.querySelector('.container')
+let container = document.querySelector('.container');
 
-for (let square = 1; square <= 256; square++) {
-    let div = document.createElement('div')
-    container.append(div)
+function squareGrid() {
+    
+    let gridSizeButton = document.getElementById('gridSize');
+     gridSizeButton.addEventListener('click', function() {
+        let userChoice = prompt('pick a size (2 to 100');
+        container.innerHTML = '';
+        createSquares(userChoice);
+        coloredSquares();
+    })
+    
+    createSquares()
+    coloredSquares()
 }
 
-let squares = document.querySelectorAll('div');
-squares.forEach(function(square) {
+squareGrid()
+
+
+
+function createSquares(amount = 16) {
+    
+    for (let square = 1; square <= amount * amount; square++) {
+        let div = document.createElement('div');
+        let squareSize = 960 / amount;
+        div.style.width = `${squareSize}px`;
+        div.style.height = `${squareSize}px`;
+        container.append(div);
+    }
+}
+
+
+
+function coloredSquares() {
+    
+    let squares = document.querySelectorAll('div');
+    squares.forEach(function(square) {
     square.addEventListener('mouseover', function() {
-        square.style.backgroundColor = 'red'
+        square.style.backgroundColor = 'gainsboro';
+       })
     })
-})
+}
+
+
+
+
+
 
 
 
